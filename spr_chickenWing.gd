3d40dd7ad5a285c2b8e6c804f9e878566327a53a
s4pu <leopab@gmx.de>
2018-03-28 19:01:46 +0200

    chicken follows spaceship when alarmed

--- spr_chickenWing.gd
+++ spr_chickenWing.gd
@@ -1,8 +1,5 @@
 extends RigidBody2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
 var speed = 50
 var player = null
 var target = null
@@ -17,6 +14,7 @@ func _physics_process(dt):
 		var delta = target.position - position
 		var movement = delta.normalized() * speed
 		applied_force = movement
+		rotation = (target.position - position).angle()
 	else:
 		var follow = get_node("../Path2D/PathFollow2D")
 		follow.loop = true
@@ -26,7 +24,6 @@ func _physics_process(dt):
 		var start = position
 		position = get_node("../Path2D").position + follow.position
 		rotation = follow.rotation
-	
 
 func _on_vision_area_body_entered(body):
 	if body is preload("res://spaceship.gd"):
