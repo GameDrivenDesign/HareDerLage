993ef520b9591ea96254df0b01956eb26c2dc844
Corinna Jaschek <corinna.jaschek@gmail.com>
2018-03-28 14:57:58 +0200

    add spaceship controls

---
+++ spaceship.gd
@@ -0,0 +1,21 @@
+extends RigidBody2D
+
+var torque = 100
+var speed = 20
+var handling = 20
+
+func _physics_process(delta):
+	
+	var angle = Vector2(0, 1).rotated(transform.get_rotation()).angle_to(linear_velocity)
+		
+	if Input.is_action_pressed("up"):
+		applied_force = Vector2(0, speed + handling * abs(angle)).rotated(transform.get_rotation())
+	else:
+		applied_force = Vector2()
+	
+	if Input.is_action_pressed("left"):
+		applied_torque = - torque
+	elif Input.is_action_pressed("right"):
+		applied_torque = torque
+	else:
+		applied_torque = 0
