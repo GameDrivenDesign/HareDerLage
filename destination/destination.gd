40210c78b4a70b53fd4e5be686f3f50e9603df55
Christian Flach <cmfcmf.flach@gmail.com>
2018-03-29 10:07:30 +0200

    Finish destination zone

--- destination/destination.gd
+++ destination/destination.gd
@@ -1,6 +1,5 @@
 extends Area2D
 
-
-func _on_destination_area_entered(area):
-	# TODO. What now.
-	pass # replace with function body
+func _on_destination_body_entered(body):
+	if body is preload("res://spaceship.gd"):
+		get_node("/root/game").win()
