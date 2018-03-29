4c602f5f9c11adc08d3756ada7d0ddc93d5802aa
Christian Flach <cmfcmf.flach@gmail.com>
2018-03-29 13:10:01 +0200

    added pulse
    Co-authored-by: Wanda <wanda.baltzer@student.hpi.de>

--- comet/comet.gd
+++ comet/comet.gd
@@ -2,6 +2,9 @@ extends RigidBody2D
 
 const dps = 5
 
+func _ready():
+	$AnimationPlayer.play("pulse", -1, rand_range(0.05, 0.1))
+	
 func _process(delta):
 	for collider in get_colliding_bodies():
 		if collider.has_method('take_damage'):
