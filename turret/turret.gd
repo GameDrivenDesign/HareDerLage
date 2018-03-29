2be93124f2fc1fdce177debf63e45dbee27e970e
Tom Beckmann <tomjonabc@gmail.com>
2018-03-29 13:09:39 +0200

    Make turret shoot faster bullets and two at a time

--- turret/turret.gd
+++ turret/turret.gd
@@ -2,7 +2,7 @@ extends Sprite
 
 var target = null
 
-const PROJECTILE_SPEED = Vector2(0, 100)
+const PROJECTILE_SPEED = Vector2(0, 500)
 const WEAPON_COOLDOWN = 0.2
 
 var cooldown = 0
@@ -33,13 +33,14 @@ func _on_vision_area_body_entered(body):
 		aim_to_target()
 
 func shoot_to_target():
-	var aimvector = target.position - position
-	
+	spawn_projectile(position + Vector2(0, -15).rotated($Polygon2D.rotation))
+	spawn_projectile(position + Vector2(0,  15).rotated($Polygon2D.rotation))
+
+func spawn_projectile(pos):
 	var projectile = preload("res://projectile.tscn").instance()
-	#projectile.position = position + Vector2(0, 20).rotated($Polygon2D.transform.get_rotation())
-	projectile.position = position
+	projectile.position = pos
 	projectile.rotation = position.angle_to_point(target.position) + PI / 2
-	projectile.direction = PROJECTILE_SPEED.rotated(projectile.rotation)
+	projectile.direction = PROJECTILE_SPEED.rotated($Polygon2D.rotation + PI / 2)
 	projectile.add_collision_exception_with($StaticBody2D)
 	
 	get_parent().add_child(projectile)
